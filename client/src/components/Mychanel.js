d59f0e25da774d44101ecdc6f5d380285cdac782
genditelamdika <gendydika@gmail.com>
2023-05-09 03:31:09 -0500

    sub

--- client/src/components/Mychanel.js
+++ client/src/components/Mychanel.js
@@ -68,7 +68,7 @@ function Mychanel() {
             <Card.Text className="text-white fs-3 mb-0">
               {profile.chanelname}
             </Card.Text>
-            <Card.Text style={{ color: "#F0F0F0" }}>120K Subscriber</Card.Text>
+            <Card.Text style={{ color: "#F0F0F0" }}>{profile.subscriber}</Card.Text>
           </Stack>
           {/* <Link to={`/Editchanel/${profile.id}`}>
                       <button  type="buton"  className="shadow  btn btn-success fw-bold  " style={{width:"70px"}} name={profile.id} value={profile.id}>
